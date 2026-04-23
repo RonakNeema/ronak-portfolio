88f1cfd4fa76677321cff6f2e13fd043c11b4cf9
Ronak Neema <neemaronak17@gmail.com>
2026-04-23 15:52:40 -0600

    changes

--- app/components/HomeContent.tsx
+++ app/components/HomeContent.tsx
@@ -36,11 +36,11 @@ export default function HomeContent() {
         {/* <Projects /> */}
         
         {/* GitHub Stats */}
-        <section className="py-20 px-6 md:px-12">
+        {/* <section className="py-20 px-6 md:px-12">
           <div className="max-w-6xl mx-auto">
             <GitHubStats />
           </div>
-        </section>
+        </section> */}
 
         {/* LeetCode Stats - Full Width */}
         {/* <section className="py-20 px-6 md:px-12 bg-[#111111]">
